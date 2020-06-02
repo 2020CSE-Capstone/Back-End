8a3466d8c01f10d431d758bc5f87e1091bdeeb7d
parkwoongki <sbe03005@naver.com>
2020-06-02 20:12:46 +0900

    [200602][ADD] Record Record MVC Added

--- Capstone/src/main/java/cse/capstonedesign/Capstone/dto/request/InsertDrinkRecordRequestDTO.java
+++ Capstone/src/main/java/cse/capstonedesign/Capstone/dto/request/InsertDrinkRecordRequestDTO.java
@@ -13,13 +13,13 @@ public class InsertDrinkRecordRequestDTO {
 
 	private double figure;
 	private int glass;
-	private Date drink_date;
+	private String drink_date;
 	private String drink_name;
 	private int user_id;
 
 	public InsertDrinkRecordRequestDTO() { }
 
-	public InsertDrinkRecordRequestDTO(double figure, int glass, Date drink_date, String drink_name, int user_id) {
+	public InsertDrinkRecordRequestDTO(double figure, int glass, String drink_date, String drink_name, int user_id) {
 		this.figure = figure;
 		this.glass = glass;
 		this.drink_date = drink_date;
